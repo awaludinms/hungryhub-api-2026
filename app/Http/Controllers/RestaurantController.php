5e7eb249679076c54f4220164736cfe529794ef8
Awaludin <awaludinr830@gmail.com>
2026-03-19 13:47:05 +0700

    + add Restaurant Controller ^ update Restaurant List + add Restaurant Resource as Response

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RestaurantStoreRequest;
+use App\Http\Resources\RestaurantResource;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return new RestaurantResource(Restaurant::paginate(10));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    /**
+     * Add restaurant menu item
+     * @param string $id
+     * @return void
+     */
+    public function menu_item(string $id)
+    {
+        // TODO: Add restaurant menu Item
+
+    }
+
+    /**
+     * List Menu Item
+     * @param string $id
+     * @return void
+     */
+    public function menu_item_list(string $id)
+    {
+        // TODO: List Menu Item support filter by category
+    }
+}
