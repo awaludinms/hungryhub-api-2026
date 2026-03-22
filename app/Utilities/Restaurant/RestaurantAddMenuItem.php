cbaec61ccc6025edb888b34b071df3e849d1c191
Awaludin <awaludinr830@gmail.com>
2026-03-22 08:48:14 +0700

    ^ add "restaurant menu add" process

---
+++ app/Utilities/Restaurant/RestaurantAddMenuItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Utilities\Restaurant;
+
+use App\Http\Resources\FailedResource;
+use App\Http\Resources\SuccessResource;
+use App\Models\MenuItem;
+use App\Models\Restaurant;
+use App\Traits\CommonResponse;
+
+class RestaurantAddMenuItem
+{
+    use CommonResponse;
+    /**
+     * Create a new class instance.
+     */
+    public function addMenu($request, $id)
+    {
+        $validated = $request->validated();
+        $validated['created_at'] = date('Y-m-d');
+
+        if (Restaurant::where('id', $id)->exists()) {
+            $validated['restaurant_id'] = $id;
+            $validated['is_available'] = ($validated['is_available']) ? 1 : 0;
+
+            try {
+                $id = MenuItem::insertGetId($validated);
+
+                return $this->success('Menu Item success saved', $validated, $id);
+
+            } catch (\Exception $e) {
+                return $this->failed('Menu Item fail to save', $e, $validated);
+            }
+
+        } else {
+            return $this->page404("Resaurant Id=$id is not exists.");
+        }
+    }
+}
