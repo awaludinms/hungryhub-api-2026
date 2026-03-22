a19f780f9de5bf1ebf46d16a0ad5725fc2625ab4
Awaludin <awaludinr830@gmail.com>
2026-03-22 08:51:36 +0700

    ^ refactor every $restaurant param into each type for validation function use

--- app/Utilities/RestaurantUtility.php
+++ app/Utilities/RestaurantUtility.php
@@ -2,30 +2,36 @@
 
 namespace App\Utilities;
 
+use App\Utilities\Restaurant\RestaurantAddMenuItem;
+use App\Utilities\Restaurant\RestaurantDetail;
+use App\Utilities\Restaurant\RestaurantList;
+use App\Utilities\Restaurant\RestaurantStore;
+use App\Utilities\Restaurant\RestaurantUpdate;
+
 class RestaurantUtility
 {
     /**
      * Create a new class instance.
      */
-    public function list($restaurants)
+    public function list(RestaurantList $restaurants)
     {
         //
         return($restaurants->list());
     }
 
-    public function store($restaurants, $request)
+    public function store(RestaurantStore $restaurants, $request)
     {
         //
         return($restaurants->store($request));
     }
 
-    public function update($restaurants, $request, $id)
+    public function update(RestaurantUpdate $restaurants, $request, $id)
     {
         //
         return($restaurants->update($request, $id));
     }
 
-    public function detail($restaurants, $id)
+    public function detail(RestaurantDetail $restaurants, $id)
     {
         //
         return($restaurants->detail($id));
@@ -37,7 +43,7 @@ class RestaurantUtility
      * @param mixed $restaurants
      * @param mixed $id
      */
-    public function addMenu($restaurants, $request, $id)
+    public function addMenu(RestaurantAddMenuItem $restaurants, $request, $id)
     {
         //
         return($restaurants->addMenu($request, $id));
